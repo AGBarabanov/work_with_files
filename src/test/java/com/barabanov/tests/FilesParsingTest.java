30223dc34077da3865b914713b96e8865b0c79ea
Anton Barabanov <anton.barabanov71995@gmail.com>
2023-09-10 20:45:08 +0300

    created test parse pdf/xls file

--- src/test/java/com/barabanov/tests/FilesParsingTest.java
+++ src/test/java/com/barabanov/tests/FilesParsingTest.java
@@ -1,5 +1,38 @@
 package com.barabanov.tests;
 
-public class FilesParsingTest {
+import com.codeborne.pdftest.PDF;
+import com.codeborne.xlstest.XLS;
+import org.junit.jupiter.api.Test;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class FilesParsingTest extends TestBase {
+
+    ClassLoader cl = FilesParsingTest.class.getClassLoader();
+
+
+
+    @Test
+    void pdfParseTest() throws Exception {
+        open("https://junit.org/junit5/docs/current/user-guide/");
+
+        File pdfDownloadedfile = $("a[href='junit-user-guide-5.10.0.pdf']").download();
+        PDF content = new PDF(pdfDownloadedfile);
+        assertThat(content.author).contains("Sam Brannen");
+    }
+
+    @Test
+    void xlsParseTest() throws Exception {
+        try(InputStream resourceAsStream = cl.getResourceAsStream("example.xlsx")){
+            XLS content = new XLS(resourceAsStream);
+            assertThat(content.excel.getSheetAt(0).getRow(1).getCell(1)).contains("example");
+        }
+    }
 }
